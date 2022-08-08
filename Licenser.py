3fd44f9edc3732f440e365e60d53f9a5c6ceaaad
KubaBoi <jakubanderle@outlook.cz>
2022-08-08 15:32:44 +0200

    debug

--- Licenser.py
+++ Licenser.py
@@ -16,6 +16,7 @@ main file of Cheese Application
 if __name__ == "__main__":
     CheeseBurger.init()
 
+    i = 0
     while True:
         try:
             req = {
@@ -24,9 +25,13 @@ if __name__ == "__main__":
                 "icon": "/icon.png",
                 "color": "FF0000"
             }
+            print("Sending request to FrogieHub")
             requests.post(f"http://localhost/services/doYouKnowMe", json=req)
             break
         except:
+            i += 1
+            if (i >= 10): break
+            print("Unauble to reach FrogieHub")
             time.sleep(1)
 
     CheeseBurger.serveForever()
